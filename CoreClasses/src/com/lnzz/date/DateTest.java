1e62d8d72af4f492b82d35700cd2e898cafd1421
yyuguang <790335255@qq.com>
2020-09-05 23:04:37 +0800

    Java核心类：JDK 8之前日期和时间的API

---
+++ CoreClasses/src/com/lnzz/date/DateTest.java
@@ -0,0 +1,68 @@
+package com.lnzz.date;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+/**
+ * ClassName：DateTest
+ *
+ * @Author 冷暖自知
+ * @Date 2020/9/5 22:57
+ * @Version 1.0
+ * @Description JDK 8之前日期和时间的API测试
+ */
+public class DateTest {
+    /**
+     * System类中的currentTimeMillis()
+     */
+    @Test
+    public void test1() {
+        long time = System.currentTimeMillis();
+        //返回当前时间与1970年1月1日0时0分0秒之间以毫秒为单位的时间差。
+        //称为时间戳
+        System.out.println(time);
+    }
+
+    /**
+     * java.util.Date类
+     *        |---java.sql.Date类
+     *
+     * 1.两个构造器的使用
+     *     >构造器一：Date()：创建一个对应当前时间的Date对象
+     *     >构造器二：创建指定毫秒数的Date对象
+     * 2.两个方法的使用
+     *     >toString():显示当前的年、月、日、时、分、秒
+     *     >getTime():获取当前Date对象对应的毫秒数。（时间戳）
+     *
+     * 3. java.sql.Date对应着数据库中的日期类型的变量
+     *     >如何实例化
+     *     >如何将java.util.Date对象转换为java.sql.Date对象
+     */
+    @Test
+    public void test2(){
+        //构造器一：Date()：创建一个对应当前时间的Date对象
+        Date date1 = new Date();
+        System.out.println(date1.toString());
+
+        System.out.println(date1.getTime());
+
+        //构造器二：创建指定毫秒数的Date对象
+        Date date2 = new Date(155030620410L);
+        System.out.println(date2.toString());
+
+        //创建java.sql.Date对象
+        java.sql.Date date3 = new java.sql.Date(35235325345L);
+        System.out.println(date3);
+
+        //如何将java.util.Date对象转换为java.sql.Date对象
+        //情况一：
+//        Date date4 = new java.sql.Date(2343243242323L);
+//        java.sql.Date date5 = (java.sql.Date) date4;
+        //情况二：
+        Date date6 = new Date();
+        java.sql.Date date7 = new java.sql.Date(date6.getTime());
+
+
+    }
+}
